a7eb7e8b6dcbbc1df424774feb2c702b7bda6496
muhammedrafi-kp <rafikpmty@gmail.com>
2025-09-24 20:26:48 +0530

    chore: remove RabbitMQ dependency

--- api-gateway/src/server.ts
+++ api-gateway/src/server.ts
@@ -2,10 +2,8 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
-import { createProxyMiddleware } from 'http-proxy-middleware'
 import { validateToken } from "./middleware/validateToken";
 import { connectToRedis } from './config/redisConfig';
-import { AuthConsumer } from './consumer/authConsumer';
 import {setupProxies} from "./proxies";
 
 configDotenv();
@@ -22,20 +20,10 @@ app.use(cors({
 app.use(morgan('dev'));
 
 connectToRedis();
-AuthConsumer.initialize();
 
 app.use(validateToken as express.RequestHandler);
 
 setupProxies(app);
-
-// app.use("/api/user-service", createProxyMiddleware({ target: process.env.USER_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/collection-service", createProxyMiddleware({ target: process.env.COLLECTION_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/payment-service", createProxyMiddleware({ target: process.env.PAYMENT_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/collection-service", createProxyMiddleware({ target: process.env.COLLECTION_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/payment-service", createProxyMiddleware({ target: process.env.PAYMENT_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/location-service", createProxyMiddleware({ target: process.env.LOCATION_SERVICE_URL, changeOrigin: true }));
-// app.use("/api/chat-service", createProxyMiddleware({ target: process.env.CHAT_SERVICE_URL, changeOrigin: true, ws: true }));
-// app.use("/api/notification-service", createProxyMiddleware({ target: process.env.NOTIFICATION_SERVICE_URL, changeOrigin: true, ws: true }));
 
 app.get('/api/health', (req, res) => {
     res.status(200).json({ status: 'ok' });
